7db1d531d1365d4565dfb201137d0a7ae1d65096
Charlie <cmdouglas@gmail.com>
2015-05-01 15:24:43 -0500

    make solver try larger tolerances until it finds a solution, also formatting

--- README.md
+++ README.md
@@ -2,13 +2,17 @@
 
 Requirements: Python 3.4 + 
 
-Usage: python3 countdown_nubers.py numbers -g goal [-t tolerance]
+Usage: python3 countdown_nubers.py [numbers -g goal]
+
+If you omit numbers and goal, it will generate a random problem and solve it.
 
 examples:
 
 	$ python3 countdown_numbers.py 25 50 75 100 3 6 -g 952
-	100 + 3 = 103
+	Trying to reach 952 with numbers [25, 50, 75, 100, 3, 6] and tolerance 0
+
 	75 * 6 = 450
+	100 + 3 = 103
 	450 / 50 = 9
 	103 * 9 = 927
 	25 + 927 = 952
@@ -16,14 +20,26 @@ examples:
 	Solution: 25 + (100 + 3) * 75 * 6 / 50 = 952
 
 	$ python3 countdown_numbers.py 25 50 75 100 3 6 -g 962
+	Trying to reach 962 with numbers [25, 50, 75, 100, 3, 6] and tolerance 0
+
 	No solution.
+	Trying to reach 962 with numbers [25, 50, 75, 100, 3, 6] and tolerance 1
 
-	$ python3 countdown_numbers.py 25 50 75 100 3 6 -g 962 -t 1
-	75 + 3 = 78
-	100 * 6 = 600
-	600 / 50 = 12
-	78 * 12 = 936
-	25 + 936 = 961
+	50 * 6 = 300
+	100 / 25 = 4
+	300 - 4 = 296
+	3 * 296 = 888
+	75 + 888 = 963
 
-	Solution: 25 + (75 + 3) * 100 * 6 / 50 = 961
+	Solution: 75 + 3 * (50 * 6 - (100 / 25)) = 963
 	(1 away)
+	
+	$ python3 countdown_numbers.py
+	Trying to reach 956 with numbers [25, 50, 8, 7, 5, 9] and tolerance 0
+
+	25 * 8 = 200
+	200 - 7 = 193
+	5 * 193 = 965
+	965 - 9 = 956
+
+	Solution: 5 * (25 * 8 - 7) - 9 = 956
--- countdown_numbers.py
+++ countdown_numbers.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python3
-
+import random
 import argparse
 from itertools import combinations
+
 from biae import BinaryIntegerArithmeticExpression as E
 from search import SearchNode, AStarSearch
+
 
 def get_result(n1, n2, operator):
     if operator == '+':
@@ -14,95 +16,93 @@ def get_result(n1, n2, operator):
         result = n1 * n2
     elif operator == '/':
         result = int(n1 / n2)
-        
+
     return result
+
 
 class CountDownNumbersException(Exception):
     pass
 
+
 class CountDownNumbers:
-    
     def __init__(self, numbers, goal):
         self.numbers = numbers
         self.goal = goal
-        
+
     def find_solution(self, tolerance=0):
         def _id(node):
             numbers = node.data['numbers']
             return tuple(sorted(numbers))
-            
-            
+
         def is_goal(node):
             numbers = node.data['numbers']
             for number in numbers:
                 if abs(number - self.goal) <= tolerance:
                     return True
-            
+
             return False
-            
+
         def heuristic(node):
             numbers = node.data['numbers']
             diffs = [abs(n - self.goal) for n in numbers]
-            return max(diffs)
-            
+            return min(diffs)
+
         def possible_moves(node):
             numbers = node.data['numbers']
             moves = []
             for (n1, n2) in combinations(numbers, 2):
-                moves.extend([
-                    (n1, '+', n2),
-                    (n1, '*', n2),
-                ])
-                
-                #disallow negative numbers
+
+                moves.append((n1, '*', n2))
+                moves.append((n1, '+', n2))
+
+                # disallow fractions
+                if n2 != 0 and n1 % n2 == 0:
+                    moves.append((n1, '/', n2))
+
+                if n1 != 0 and n2 % n1 == 0:
+                    moves.append((n2, '/', n1))
+
+
+                # disallow negative numbers
                 if n1 >= n2:
                     moves.append((n1, '-', n2))
                 else:
                     moves.append((n2, '-', n1))
-                
-                #disallow fractions
-                if n2 != 0 and n1 % n2 == 0:
-                    moves.append((n1, '/', n2))
-                    
-                if n1 != 0 and n2 % n1 == 0:
-                    moves.append((n2, '/', n1))
-                    
+
             return moves
-            
+
         def apply_move(node, move):
-            n1, operator, n2 = move;
+            n1, operator, n2 = move
             numbers = node.data['numbers'][:]
             numbers.remove(n1)
             numbers.remove(n2)
-            
+
             result = get_result(n1, n2, operator)
-            
+
             numbers.append(result)
             data = {
                 'numbers': numbers
             }
-            
+
             return SearchNode(data, _id, possible_moves, apply_move)
-            
-            
+
         data = {
             'numbers': self.numbers
         }
-        
+
         start_node = SearchNode(data, _id, possible_moves, apply_move)
-        
+
         search = AStarSearch(start_node, is_goal, heuristic)
-        
+
         path = search.do_search()
-        
+
         if not path:
             raise CountDownNumbersException("No path found.")
-            
-            
+
         return path, self.moves_to_expression(path)
-            
+
     def moves_to_expression(self, moves):
-        
+
         def extract_expression(expressions, val):
             to_extract = None
             for e in expressions:
@@ -111,57 +111,103 @@ class CountDownNumbers:
                     break
             expressions.remove(to_extract)
             return to_extract
-            
+
         expressions = [E(n) for n in self.numbers]
         for move in moves:
             n1, op, n2 = move
             e1 = extract_expression(expressions, n1)
             e2 = extract_expression(expressions, n2)
-            
+
             result = E(op1=e1, op2=e2, o=op)
             expressions.append(result)
-        
-        mindiff, expression = min([(abs(e.value-self.goal), e) for e in expressions])
-        
+
+        mindiff, expression = min([
+            (abs(e.value-self.goal), e) for e in expressions
+        ])
+
         return expression
-        
+
 
 def report_path(path):
     for move in path:
         n1, operator, n2 = move
-                
-        print ("{n1} {operator} {n2} = {val}".format(
+
+        print("{n1} {operator} {n2} = {val}".format(
             n1=n1,
             n2=n2,
             operator=operator,
             val=get_result(n1, n2, operator)
         ))
-                
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("numbers", type=int, nargs='+', help="the numbers used to find the goal")
-    parser.add_argument("-g", "--goal", type=int, help="the goal")
-    parser.add_argument("-t", "--tolerance", default=0, type=int, nargs="?", help="the error tolerance, i.e. 1 will allow for solutions +/1 1 from the goal")
+
+
+def generate_problem(nlarge=1):
+    assert 0 <= nlarge <= 4
+    nsmall = 6 - nlarge
+
+    large = [25, 50, 75, 100]
+    small = 2 * list(range(1, 11))
+
+    numbers = random.sample(large, nlarge) + random.sample(small, nsmall)
+    goal = random.randrange(101, 1000)
+
+    return numbers, goal
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Countdown numbers solver.")
+    parser.add_argument(
+        "numbers",
+        type=int,
+        nargs='*',
+        help="the numbers used to find the goal"
+    )
+    parser.add_argument(
+        "-g",
+        "--goal",
+        type=int,
+        nargs="?",
+        default=-1,
+        help="the goal"
+    )
     args = parser.parse_args()
-    
-    c = CountDownNumbers(args.numbers, args.goal)
-    
-    try:
-        path, expression = c.find_solution(tolerance=args.tolerance)
-        report_path(path)
-        print("\nSolution: {e} = {v}".format(e=expression, v=expression.value))
-        
-        error = abs(expression.value - c.goal)
-        if error > 0:
-            print ("({error} away)".format(error=error))
-        
-        
-    except CountDownNumbersException as e:
-        print("No solution.")
-        
-    
-    
-    
-    
-        
-            
+
+    numbers = args.numbers
+    goal = args.goal
+
+    if (not numbers) and (goal == -1):
+        numbers, goal = generate_problem(
+            nlarge=random.randrange(0, 5)
+        )
+
+    c = CountDownNumbers(numbers, goal)
+    tolerance = 0
+    success = False
+
+    while not success:
+        print(
+            "Trying to reach {g} with numbers {n} and tolerance {t}\n".format(
+                n=numbers,
+                g=goal,
+                t=tolerance
+            )
+        )
+
+        try:
+            path, expression = c.find_solution(tolerance)
+            report_path(path)
+            print("\nSolution: {e} = {v}".format(
+                e=expression, v=expression.value
+            ))
+
+            error = abs(expression.value - c.goal)
+            if error > 0:
+                print("({error} away)".format(error=error))
+
+            success = True
+
+        except CountDownNumbersException:
+            print("No solution.")
+            tolerance += 1
+
+if __name__ == '__main__':
+    main()
